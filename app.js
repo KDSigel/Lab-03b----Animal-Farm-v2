157867f2515fd3dc43169026d23c5ed4b624b269
Karl Sigel <heykarl@gmail.com>
2021-08-12 09:20:43 -0700

    added animals and stuff

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-// import functions and grab DOM elements
 
-// initialize state
+import { theHorseSoundAndText, theDogSoundAndText, theCatSoundAndText } from './utils.js';
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const horseEl = document.getElementById('horse-image');
+const dogEl = document.getElementById('dog-image');
+const catEl = document.getElementById('cat-image');
+
+horseEl.addEventListener('click', () => {
+    theHorseSoundAndText();
+});
+
+dogEl.addEventListener('click', () => {
+    theDogSoundAndText();
+});
+
+catEl.addEventListener('click', () => {
+    theCatSoundAndText();
+});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'd') {
+        theDogSoundAndText();
+    }});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'h') {
+        theHorseSoundAndText();
+    }});
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'c') {
+        theCatSoundAndText();
+    }});
