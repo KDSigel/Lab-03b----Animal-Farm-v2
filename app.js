5af4b8d7ec0333a87ece0a9ca1dff1c0be07dae0
Karl Sigel <heykarl@gmail.com>
2021-08-14 20:41:17 -0700

    added celebs

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 import { playSoundAndChangeText } from './utils.js';
 
-const horseEl = document.getElementById('horse-image');
-const dogEl = document.getElementById('dog-image');
-const catEl = document.getElementById('cat-image');
+const horseEl = document.getElementById('hoss-image');
+const dogEl = document.getElementById('paris-image');
+const catEl = document.getElementById('sheen-image');
 
 const horseSound = document.getElementById('horse-sound');
 const dogSound = document.getElementById('dog-sound');
 const catSound = document.getElementById('cat-sound');
 
   horseEl.addEventListener('click', () => {
-    playSoundAndChangeText(horseSound, 'This is a horse');
+    playSoundAndChangeText(horseSound, 'This is Hoff');
   });
 
   dogEl.addEventListener('click', () => {
-    playSoundAndChangeText(dogSound, 'This is a dog');
+    playSoundAndChangeText(dogSound, 'This is Paris');
   });
 
   catEl.addEventListener('click', () => {
-    playSoundAndChangeText(catSound, 'This is a cat. If it were the size of a dog or horse, it would kill you.');
+    playSoundAndChangeText(catSound, 'This is Charlie');
   });
 
   document.addEventListener('keydown', function(event) {
     if (event.key === 'd') {
-        playSoundAndChangeText(horseSound, 'This is a horse');
+        playSoundAndChangeText(horseSound, 'This is Hoff');
     }});
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'h') {
-        playSoundAndChangeText(dogSound, 'This is a dog');
+        playSoundAndChangeText(dogSound, 'This is Paris');
     }});
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'c') {
-       playSoundAndChangeText(catSound, 'This is a cat. If it were the size of a dog or horse, it would kill you.');
+       playSoundAndChangeText(catSound, 'This is Charlie');
     }});
